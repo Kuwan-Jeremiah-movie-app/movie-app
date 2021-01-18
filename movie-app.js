b3dd037577e25235869a150fee784ca5df345ef2
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-17 21:05:00 -0600

    functioning delete button

--- movie-app.js
+++ movie-app.js
@@ -18,21 +18,14 @@ let title = $("#addMovie").val();
 let rating = $("#addRating").val()
 let genre = $("#addGenre").val()
 let year = $("#addYear").val()
-// const movieObj = {
-//     "Title ": title,
-//     "Rating ": rating
-// };
-const options = {
-    method: 'POST',
-}
-// console.log(movieObj);
+
 $("#addNew").click(function (e) {
     e.preventDefault();
     let title = $("#addMovie").val()
     let rating = $("#addRating").val()
     let genre = $("#addGenre").val()
     let year = $("#addYear").val()
-    if(title === "" || rating === "" || genre === "" || year === ""){
+    if (title === "" || rating === "" || genre === "" || year === "") {
         $(".addGroup input").addClass("required")
     } else {
         const movieObj = {
@@ -50,7 +43,6 @@ $("#addNew").click(function (e) {
             body: JSON.stringify(movieObj),
         };
         fetch(movieApiUrl, options).then(function (response) {
-// console.log(response)
             response.json().then(function (newMovie) {
                 console.log(newMovie)
             })
@@ -61,46 +53,22 @@ $("#addNew").click(function (e) {
 })
 $.ajax(movieApiUrl).done(function (data) {
     data.forEach(function (movie) {
-        let movieHtml = "<div class='float-left'> <div class=\"card \" style=\"width: 18rem;\">";
-        movieHtml += `<img src='${movie.poster}' style="width: 100px; height: 100px" alt="" class="d-flex justify-content-between">`
-        movieHtml += `<div class="card-body">${movie.title.toUpperCase()}</div>`
+        let movieHtml = ` <div class="card float-left" style="width: 18rem">`;
+        movieHtml += `<img src='${movie.poster}' style="width: 100px; height: 100px" class="d-flex justify-content-between" alt="poster">`
+        movieHtml += `<h3 class="card-body">${movie.title.toUpperCase()}</h3>`
         movieHtml += `<h5>${movie.genre}</h5>`
         movieHtml += `<p>Rating: ${movie.rating}</p>`
         movieHtml += `<p>Year: ${movie.year}</p>`
-        movieHtml += `<button type="button" class="btn btn-primary" id="${movie.id}" data-toggle="modal"  data-movie="${movie.id}" data-target="#exampleModal">
+        movieHtml += `<div><button type="button" class="btn btn-primary" id="${movie.id}" data-toggle="modal"  data-movie="${movie.id}" data-target="#exampleModal">
         Edit Movie
     </button>`
-        movieHtml += `<button type="button" class="btn red btn-danger " id="delete"  data-movie="${movie.id}" data-target="deleteMovie">
+        movieHtml += `<button type="button" class="btn btn-danger" id="${movie.id}" data-toggle="modal"  data-movie="${movie.id}" data-target="#deleteModal">
         Delete Movie
     </button></div></div>`
-
         $("#movies").append(movieHtml)
-
-
     })
-
 });
-// function editMovie(e){
-//     e.preventDefault();
-//
-// $('#close').on('hide.bs.modal',$.ajax(movieApiUrl).done(function (data) {
-//     data.forEach(function (movie) {
-//         let movieHtml = "<div> <div class=\"card \" style=\"width: 18rem;\">";
-//         movieHtml += `<img src='${movie.poster}' style="width: 100px; height: 100px" alt="">`
-//         movieHtml += `<div class="card-body">${movie.title.toUpperCase()}</div>`
-//         movieHtml += `<h5>${movie.genre}</h5>`
-//         movieHtml += `<p>Rating: ${movie.rating}</p>`
-//         movieHtml += `<p>Year: <br>${movie.year}</p></div>`
-//         movieHtml += `<button type="button" class="btn btn-primary" id="${movie.id}" data-toggle="modal"  data-movie="${movie.id}" data-target="#exampleModal">
-//         Edit Movie
-//     </button><br><br><br></div>`
-//
-//         $("#movies").append(movieHtml)
-//
-//
-//     })
-//
-// }));
+
 $('#exampleModal').on('shown.bs.modal', function (e) {
     let clickedButton = e.relatedTarget;
     let movieId = clickedButton.getAttribute('data-movie');
@@ -112,71 +80,64 @@ $('#exampleModal').on('shown.bs.modal', function (e) {
 // // //update radio buttons
 $('#saveChanges').click(function (e) {
     e.preventDefault();
-    if(title === "" || rating === "" || genre === "" || year === ""){
-        $(".modal-body input").addClass("required")
-    } else {
-        const edit = {
-            "title": document.querySelector('#addMovieModal').value,
-            "genre": document.querySelector('#addGenreModal').value,
-            "rating": document.querySelector('#addRatingModal').value,
-            "year": document.querySelector('#addYearModal').value,
-            "id": document.querySelector('#saveChanges').getAttribute('data-movie')
-        }
-        // const movieApiUrl = "https://antique-innate-coreopsis.glitch.me/movies"
-        // fetch(movieApiUrl).then(function (response) {
-        //   response.json().then(function (movies) {
-        //       console.log(movies)
-        //   })
-        // })
-        // .searchID = movies.data.id;
-        const movieObj = {"title": title, "rating": rating, "genre": genre, "year": year};
-        const patchMethod = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(edit),
-        };
-        fetch(movieApiUrl + "/" + edit.id, patchMethod).then(function (response) {
-            console.log(response);
-            // console.log(data[0]);
-        })
-    }
-})
-
-$('#deleteMovie').click(function (e) {
-    e.preventDefault();
-    let edit = {
+    const edit = {
         "title": document.querySelector('#addMovieModal').value,
         "genre": document.querySelector('#addGenreModal').value,
         "rating": document.querySelector('#addRatingModal').value,
         "year": document.querySelector('#addYearModal').value,
         "id": document.querySelector('#saveChanges').getAttribute('data-movie')
     }
-
-
-// Make the HTTP Delete call using fetch api
-    fetch(movieApiUrl + "/" + edit.id, {
-        method: 'DELETE', // Method itself
-        // headers: {
-        //     'Content-type': 'application/json; charset=UTF-8' // Indicates the content
-        // },
-        // body: JSON.stringify(edit),
-
-        // No need to have body, because we don't send nothing to the server.
+    const movieApiUrl = "https://antique-innate-coreopsis.glitch.me/movies"
+    // fetch(movieApiUrl).then(function (response) {
+    //   response.json().then(function (movies) {
+    //       console.log(movies)
+    //   })
+    // })
+    // .searchID = movies.data.id;
+    // const movieObj = {"title": title, "rating": rating, "genre": genre, "year": year};
+    const patchMethod = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(edit),
+    };
+    fetch(movieApiUrl + "/" + edit.id, patchMethod).then(function (response) {
+        console.log(response);
+        // console.log(data[0]);
     })
-        .then(res => res.text())
-        .then(res => console.log(res))
+})
+$('#deleteModal').on('shown.bs.modal', function (e) {
+    let clickedButtonDelete = e.relatedTarget;
+    let movieIdDelete = clickedButtonDelete.getAttribute('data-movie');
+    // This is the id of the movie. So now, I can set this in the form.
+    console.log(movieIdDelete);
+    // Find the modal, and set this data attribute
+    $('#deleteModal button#confirmDelete').attr('data-movie', movieIdDelete);
+})
+$("#confirmDelete").click(function (e) {
+    e.preventDefault();
+    let remove ={
+        "title": document.querySelector('#addMovieModal').value,
+        "genre": document.querySelector('#addGenreModal').value,
+        "rating": document.querySelector('#addRatingModal').value,
+        "year": document.querySelector('#addYearModal').value,
+        "id": document.querySelector('#confirmDelete').getAttribute('data-movie')
+    }
+
+    const deleteMethod = {
+        method: 'DELETE', // Method itself
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify(remove),
+    };
+// Make the HTTP Delete call using fetch api
+    fetch(movieApiUrl+"/"+ remove.id, deleteMethod)
+        .then(response => response.json())
+        .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+        .catch(err => console.log(err)) // Do something with the error
 })
 
-//
-// $("#edit").click(function () {
-//
-//         fetch(movieApiUrl +  patchMethod).then(function (response) {
-//             response.json().then(function (toEdit) {
-//                 console.log(movies)
-//             })
-//
-//         })
-//     });
-// })
+
+
